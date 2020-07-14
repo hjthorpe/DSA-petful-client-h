0fef17608d59c6a9b28f2d8b470731e9d460ec5c
Hannah Thorpe <hthorpe@protonmail.com>
2020-07-14 10:27:05 -0400

    spelling error

--- src/route/landingPage/LandingPage.js
+++ src/route/landingPage/LandingPage.js
@@ -16,7 +16,7 @@ export default class LandingPage extends React.Component {
         <p>name: Doogie</p>
         <p>story: Owner Passed away</p>
         <p>Welcome to Petful!:
-            This shelter is based on a first come first server basis, FOR THE ANIMALS NOT YOU YOU GREEDY HUMAN. You're adopiton options are limited to the pets that have had the longest stay at the shelter out of all the pets in the shelter at the time you are visiting. Please move onto the adoption page and decide if you would like to adopt one of the wonderful pets we currently have available to you. Additionally, you must wait your turn in line before you will be presented with the ability to adopt.
+            This shelter is based on a first come first server basis, FOR THE ANIMALS NOT YOU YOU GREEDY HUMAN. You're adoption options are limited to the pets that have had the longest stay at the shelter out of all the pets in the shelter at the time you are visiting. Please move onto the adoption page and decide if you would like to adopt one of the wonderful pets we currently have available to you. Additionally, you must wait your turn in line before you will be presented with the ability to adopt.
         </p>
         <Link className='link' to={"/adoption"}>Start the adoption process</Link>
       </div>
